747e795203030f02a97aa2d1b7d1455ab900cc7f
vanesaromantrujillo <62844614+vanesaromantrujillo@users.noreply.github.com>
2021-05-07 19:04:22 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -83,7 +83,7 @@ function happyDay(timestamp){
 function dayPrediction(timestamp){
     let date= new Date(timestamp *1000);
     let day = date.getDay();
-    let days = ["Sun.","Mon.", "Tue.", "Wen.", "Thu.", "Fri.", "Sat."];
+    let days = ["Sun.","Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."];
     return days[day];
 }
 function displayForecast(response){
